14caea6a805cbde6186974711fa7b7bf05cf4c9e
sverona <sasham@fastmail.com>
2019-05-30 13:15:02 -0500

    [html] think the needed root id changed?

--- redirect.js
+++
@@ -1,6 +0,0 @@
-const params = new URLSearchParams(window.location.search);
-const redirectURL = params.get('site');
-
-if (redirectURL != null) {
-  setTimeout(function() {window.location.href = redirectURL;}, 60 * 1000);
-}
